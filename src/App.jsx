db70596a7cd8e3c2f26188bb71831ed5c216f13d
Kunal Pal <124446202+kunalpal97@users.noreply.github.com>
2024-09-24 20:14:20 +0530

    Added dark light mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ExpenseProvider } from './context/ExpenseContext';
 import Home from './pages/Home';
+import DarkModeToggle from './components/DarkModeToggle'; // Import the toggle button
 
 const App = () => {
   return (
     <ExpenseProvider>
       <Router>
+        <DarkModeToggle /> {/* Add the toggle button */}
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
